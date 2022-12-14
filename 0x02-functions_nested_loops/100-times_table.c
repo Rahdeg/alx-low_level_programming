a751e13fe2cf059883a5d7eda173e6fb82109a22
Rahdeg <walett95@gmail.com>
2022-12-14 03:26:03 -0800

    time table2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,6 @@
  * @n: integer to return
  *
  */
-
 void print_times_table(int n)
 {
 	int i;
@@ -22,24 +21,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar('0');
-				}
-				else if (prod <= 9)
+				} else if (prod <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + prod);
-				}
-				else if (prod > 99)
+				} else if (prod > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar('0' + (prod / 100));
 					_putchar('0' + ((prod / 10) % 10));
 					_putchar('0' + (prod % 10));
-				}
-				else
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
