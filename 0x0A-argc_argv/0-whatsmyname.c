9a9353b3711660e92226d41e8ed733c1c44f35b2
Rahdeg <walett95@gmail.com>
2023-01-06 15:53:10 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: the int
+ * @argv: the chaine of chaine
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
