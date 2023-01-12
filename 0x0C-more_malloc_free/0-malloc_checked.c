4f87ee0fc91ecbd685d06a3c7969c7fb6b89cc3e
Rahdeg <walett95@gmail.com>
2023-01-12 15:14:55 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * malloc_checked - a function
+ * @b: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
